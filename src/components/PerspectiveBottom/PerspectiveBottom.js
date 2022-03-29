dd14e5374449a6951087350e4a6f26ef728067fe
arunsharma4084 <97881862+arunsharma4084@users.noreply.github.com>
2022-03-29 09:01:56 +0530

    adding new files

---
+++ src/components/PerspectiveBottom/PerspectiveBottom.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './PerspectiveBottom.module.css';
+
+const PerspectiveBottom = () => {
+  return (
+    <div className={styles.perspectiveBottom}>
+     
+    </div>
+  );
+};
+
+export default PerspectiveBottom;
